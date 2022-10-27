aef010574b3fb6732c04bffe77ec338f8dd56d7d
Lara Bucher <bucher.lara@web.de>
2022-10-27 12:14:05 +0200

    create veggie-page and dessert-page

---
+++ pages/Desserts/index.js
@@ -0,0 +1,8 @@
+export default function Desserts() {
+  return (
+    <>
+      <h1>These are the yummy Desserts</h1>
+      <p>lovely vegan desserts, like carrotcake, muffins and donuts</p>
+    </>
+  );
+}
---
+++ pages/Veggie/index.js
@@ -0,0 +1,8 @@
+export default function Veggie() {
+  return (
+    <>
+      <h1> This is the veggie Page </h1>
+      <p>carrot, brokkoli and potatos</p>
+    </>
+  );
+}
